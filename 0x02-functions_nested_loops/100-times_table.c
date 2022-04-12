b98e2b1ce1bc8a563ed769b03939909d209bd8be
thomas1y <thomasyohannes15@gmail.com>
2022-04-12 11:41:38 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,44 +1,60 @@
 #include "main.h"
-
 /**
- * print_times_table - Prints the times table of the input,
- *                     starting with 0.
- * @n: The value of the times table to be printed.
+ *print_times_table - prints timestable from n
+ *
+ *@n:input
+ *
+ *Return: void
+ *
  */
 void print_times_table(int n)
 {
-	int num, mult, prod;
+	int result;
 
-	if (n >= 0 && n <= 15)
+	if (n <= 15 && n > 0)
 	{
-		for (num = 0; num <= n; num++)
+		int i;
+
+		for (i = 0; i <= n; i++)
 		{
-			_putchar('0');
+			int j;
 
-			for (mult = 1; mult <= n; mult++)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar(',');
-				_putchar(' ');
-
-				prod = num * mult;
-
-				if (prod <= 99)
-					_putchar(' ');
-				if (prod <= 9)
-					_putchar(' ');
-
-				if (prod >= 100)
+				result = i * j;
+				if (j == 0)
 				{
-					_putchar((prod / 100) + '0');
-					_putchar(((prod / 10)) % 10 + '0');
+					_putchar('0' + result);
 				}
-				else if (prod <= 99 && prod >= 10)
+				else if (result >= 10)
 				{
-					_putchar((prod / 10) + '0');
+					_putchar(' ');
+					_putchar('0' + (result / 10));
+					_putchar('0' + (result % 10));
 				}
-				_putchar((prod % 10) + '0');
+				else if (result >= 100)
+				{
+					_putchar(' ');
+					_putchar('0' + (result / 100));
+					_putchar('0' + ((result / 10) % 10));
+					_putchar('0' + (result % 10));
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(result + '0');
+				}
+				if (j != n)
+				{
+					_putchar(',');
+				}
 			}
 			_putchar('\n');
 		}
+	}
+	else
+	{
+		_putchar('\n');
 	}
 }
