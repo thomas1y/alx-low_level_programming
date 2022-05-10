b5d51d8bcc26a714cd777dbf085d38883a04cfa8
thomas1y <thomasyohannes15@gmail.com>
2022-05-10 13:43:00 -0700

    task 5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,4 +1,4 @@
-#include "dog.h"
+#include "dog.h
 #include <stdlib.h>
 /**
  * free_dog - frees memory of structure dogs
@@ -14,4 +14,5 @@ void free_dog(dog_t *d)
 		free(d->name);
 		free(d->owner);
 		free(d);
-	
+	}
+}
